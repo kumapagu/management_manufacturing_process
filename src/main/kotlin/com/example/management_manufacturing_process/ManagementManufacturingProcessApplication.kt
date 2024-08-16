202035985e24c25be60418dc3fff931a8d8cd04e
kumagainaoyuki <kumagainaoyuki@team-lab.com>
2024-08-16 15:42:06 +0900

    add:kotolin-project

---
+++ src/main/kotlin/com/example/management_manufacturing_process/ManagementManufacturingProcessApplication.kt
@@ -0,0 +1,11 @@
+package com.example.management_manufacturing_process
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManagementManufacturingProcessApplication
+
+fun main(args: Array<String>) {
+	runApplication<ManagementManufacturingProcessApplication>(*args)
+}
