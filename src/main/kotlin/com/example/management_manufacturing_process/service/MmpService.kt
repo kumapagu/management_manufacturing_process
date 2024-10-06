07bb8e5451b33c695e7286bc353b2ca2279f3c2a
kumagainaoyuki <kumagainaoyuki@team-lab.com>
2024-10-06 14:56:25 +0900

    feat:サービスを追加

---
+++ src/main/kotlin/com/example/management_manufacturing_process/service/MmpService.kt
@@ -0,0 +1,10 @@
+package com.example.management_manufacturing_process.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class MmpService {
+    fun getCases(): String {
+        return "Test"
+    }
+}
