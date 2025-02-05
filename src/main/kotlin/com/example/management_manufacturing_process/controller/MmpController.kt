711c22ff5ed89246c0ae6a774938a3d4ad4c98b5
kumagainaoyuki <kumagainaoyuki@team-lab.com>
2025-02-06 00:44:30 +0900

    doc:KDocを追加

--- src/main/kotlin/com/example/management_manufacturing_process/controller/MmpController.kt
+++ src/main/kotlin/com/example/management_manufacturing_process/controller/MmpController.kt
@@ -9,12 +9,23 @@ import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
+/**
+ * 案件管理コントローラ
+ *
+ * @author kumagainaoyuki
+ */
 @RestController
 @RequiredArgsConstructor
 class MmpController {
     @Autowired
     lateinit var mmpService: MmpService
 
+    /**
+     * クライアント一覧取得
+     *
+     * @param nameKana クライアント名カナ(任意)
+     * @return クライアント一覧
+     */
     @GetMapping("/clients")
     fun getClients(
         @RequestParam(value = "nameKana", required = false, defaultValue = "") nameKana: String
@@ -22,6 +33,12 @@ class MmpController {
         return mmpService.getClients(nameKana)
     }
 
+    /**
+     * クライアント取得
+     *
+     * @param id クライアントID
+     * @return クライアント
+     */
     @GetMapping("/clients/{id}")
     fun getClient(
         @PathVariable id: Long
@@ -29,6 +46,12 @@ class MmpController {
         return mmpService.getClient(id)
     }
 
+    /**
+     * クライアント登録
+     *
+     * @param clientRegistrationForm クライアント登録フォーム
+     * @return クライアント
+     */
     @PostMapping("/clients")
     @ResponseStatus(HttpStatus.CREATED)
     fun registerClient(
@@ -40,6 +63,13 @@ class MmpController {
         )
     }
 
+    /**
+     * クライアント更新
+     *
+     * @param id クライアントID
+     * @param clientRegistrationForm クライアント登録フォーム
+     * @return クライアント
+     */
     @PutMapping("/clients/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun updateClient(
@@ -53,6 +83,11 @@ class MmpController {
         )
     }
 
+    /**
+     * クライアント削除
+     *
+     * @param id クライアントID
+     */
     @DeleteMapping("/clients/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteClient(
--- src/main/kotlin/com/example/management_manufacturing_process/service/MmpService.kt
+++ src/main/kotlin/com/example/management_manufacturing_process/service/MmpService.kt
@@ -6,10 +6,22 @@ import io.micrometer.common.util.StringUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+/**
+ * 案件管理サービス
+ *
+ * @author kumagainaoyuki
+ */
 @Service
 class MmpService {
     @Autowired
     lateinit var clientRepository: ClientRepository
+
+    /**
+     * クライアント一覧取得
+     *
+     * @param nameKana クライアント名カナ(任意)
+     * @return クライアント一覧
+     */
     fun getClients(nameKana: String): List<ClientEntity> {
         if (StringUtils.isBlank(nameKana)) {
             return clientRepository.findByOrderById()
@@ -17,15 +29,36 @@ class MmpService {
         return clientRepository.findByNameKanaContainingOrderByIdAsc(nameKana)
     }
 
+    /**
+     * クライアント取得
+     *
+     * @param id クライアントID
+     * @return クライアント
+     */
     fun getClient(id: Long): ClientEntity {
         return clientRepository.getReferenceById(id)
     }
 
+    /**
+     * クライアント登録
+     *
+     * @param name クライアント名
+     * @param nameKana クライアント名カナ
+     * @return クライアント
+     */
     fun registerClient(name: String, nameKana: String): ClientEntity {
         val clientEntity = ClientEntity(name = name, nameKana = nameKana)
         return clientRepository.save(clientEntity)
     }
 
+    /**
+     * クライアント更新
+     *
+     * @param id クライアントID
+     * @param name クライアント名
+     * @param nameKana クライアント名カナ
+     * @return クライアント
+     */
     fun updateClient(id: Long, name: String, nameKana: String): ClientEntity {
         val clientEntity = clientRepository.getReferenceById(id)
         clientEntity.name = name
@@ -33,6 +66,11 @@ class MmpService {
         return clientRepository.save(clientEntity)
     }
 
+    /**
+     * クライアント削除
+     *
+     * @param id クライアントID
+     */
     fun deleteClient(id: Long) {
         clientRepository.deleteById(id)
     }
