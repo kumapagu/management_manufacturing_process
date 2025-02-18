51e1e22514508fcf1acf96a3ec9e26a01f3b9d88
kumagainaoyuki <kumagainaoyuki@team-lab.com>
2025-02-19 00:01:59 +0900

    refactor:コントローラの説明を修正

--- src/main/kotlin/com/example/management_manufacturing_process/controller/ClientController.kt
+++ src/main/kotlin/com/example/management_manufacturing_process/controller/ClientController.kt
@@ -10,7 +10,7 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
 /**
- * 案件管理コントローラ
+ * クライアントコントローラ
  *
  * @author kumagainaoyuki
  */
